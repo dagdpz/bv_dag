2a400963c555e3cd3011690ed33b57378521f652
peterneumann <45561899+peterneumann@users.noreply.github.com>
2019-07-15 11:24:32 +0200

    Update mat2prt_reach_decision_pilot.m

--- monkeypsych_mat2prt/mat2prt_reach_decision_pilot.m
+++ monkeypsych_mat2prt/mat2prt_reach_decision_pilot.m
@@ -5,169 +5,316 @@ if nargin < 2,
     run_name = '';
 end
 
+% mat_file = ('Y:\Personal\Peter\Data\IVSK\20190620\IVSK2019-06-20_17.mat')
+% run_name = '';
+
 [pathname,filename,ext] = fileparts(mat_file);
 load(mat_file, '-mat');
 
 %% task timing parameters
 
-cue = 1000; % cue duration in ms
-mem = 7000; % duration of memory period in ms, starting from 7 s after beginning of memory: -> [onset + 7000 onset + 14000]
-% mov = 7; % [onset of state 9 onset of state 10 + 200 ms]
+cue_onset_delay = 0; %cue is roughly 200 ms long (sometimes 214, 213 etc.)
+cue_offset_delay = 800;
+
+% duration of memory period in ms, starting from 7 s after beginning of memory: -> [onset + 7000 onset + 14000]
+mem_onset_delay = 7000; % delay is 15 s long
+mem_offset_delay = -1000;
+
+% [onset of state 9 onset of state 10 + 200 ms] (you add 200 ms
+% because of estimated longer duration to reach the final destination in
+% the target (state 10 starts when crossing the border of the target)
+mov_onset_delay = 0; 
+mov_offset_delay = 200;
+
 
 %% this is manual part - defining condition names and etc.
 
 NrofPreds = 3*8; % 3 events of interest, 8 trial types
 
-% red - saccade, green - reach 
-% uisetcolor
 
-prtpreds(1).name = 'cue sac instr l';
-prtpreds(1).r = 255*1; %189;
-prtpreds(1).g = 255; %183;
-prtpreds(1).b = 100; %107;
+%% CHANGE "TRIAL" TO GET conditions
 
-prtpreds(2).name = 'del sac instr l';
-prtpreds(2).r = 218;
-prtpreds(2).g = 165;
-prtpreds(2).b = 32;
+[trial.target_chosen] = deal([]); % preallocate trial.target_chosen
+[trial.choice] = deal([]);
+[trial.eff_name] = deal([]); 
+[trial([trial.effector]==3).eff_name] = deal({'eye'});
+[trial([trial.effector]==4).eff_name] = deal({'hand'});
 
-prtpreds(3).name = 'mov sac instr l';
-prtpreds(3).r = 255; %221;
-prtpreds(3).g = 150; %160;
-prtpreds(3).b = 150; %221;
-
-prtpreds(4).name = 'post memory right';
-prtpreds(4).r = 188;
-prtpreds(4).g = 143;
-prtpreds(4).b = 143;
-
-prtpreds(5).name = 'memory left';
-prtpreds(5).r = 100; %176;
-prtpreds(5).g = 180; %224;
-prtpreds(5).b = 255; %230;
-
-prtpreds(6).name = 'post memory left';
-prtpreds(6).r = 152;
-prtpreds(6).g = 251;
-prtpreds(6).b = 152;
-
-prtpreds(7).name = 'reward';
-prtpreds(7).r = 0;
-prtpreds(7).g = 0;
-prtpreds(7).b = 255;
-
-prtpreds(8).name = 'abort';
-prtpreds(8).r = 255;
-prtpreds(8).g = 0;
-prtpreds(8).b = 0;
-
-% [prtpreds(1:NrofPreds).n_correct_trials] = deal([]); % empty array to be filled in cumulatively
-[prtpreds(1:NrofPreds).onset] = deal([]);
-[prtpreds(1:NrofPreds).offset] = deal([]);
-
-%% trial conditions, states, and state onsets
-idx_success = find([trial.success] == 1); % successful trials
-idx_abort = find([trial.success] == 0); % aborted trials
-idx_fix = find([trial.type] == 1); % fixation trials
-idx_fix_success = intersect(idx_success, idx_fix); % successful fixation trials
-idx_mem = find([trial.type] == 3); % memory saccade trials
-idx_mem_success = intersect(idx_success, idx_mem); % successful memory saccade trials
-idx_stim = find([trial.microstim] == 1); % stimulation trials
-idx_nostim = find([trial.microstim] == 0); % no-stimulation trials
-idx_fix_stim = intersect(idx_fix_success, idx_stim); % successful fixation trials with microstimulation
-idx_fix_nostim = intersect(idx_fix_success, idx_nostim); % successful fixation trials without microstimulation
-
-% get target positions
-target_selected_temp=cat(1,trial(idx_mem_success).target_selected);
-target_selected =target_selected_temp(:,1); % eye
-eye = cat(1,trial(idx_mem_success).eye);
-tar = cat(1,eye.tar);
-tar = tar(sub2ind(size(tar),[1:size(tar,1)],target_selected')); % sub2ind is important function converting subscripts to linear index
-pos = cat(1,tar.pos);
-target_left = double(pos(:,1)<0);
-
-idx_mem_left = idx_mem_success(target_left == 1); % successful memory saccades to the left
-idx_mem_right = idx_mem_success(target_left == 0); % successful memory saccades to the right
-idx_mem_left_stim = intersect(idx_mem_left, idx_stim); % successful memory saccades to the left with microstimulation
-idx_mem_left_nostim = intersect(idx_mem_left, idx_nostim); % successful memory saccades to the left without microstimulation
-idx_mem_right_stim = intersect(idx_mem_right, idx_stim); % successful memory saccades to the right with microstimulation
-idx_mem_right_nostim = intersect(idx_mem_right, idx_nostim); % successful memory saccades to the right without microstimulation
-
-% get states and state onsets for fixation and memory trials
-states_fix_stim = cat(1, trial(idx_fix_stim).states);
-states_onset_fix_stim = round(cat(1, trial(idx_fix_stim).states_onset)*1000);
-states_fix_nostim = cat(1, trial(idx_fix_nostim).states);
-states_onset_fix_nostim = round(cat(1, trial(idx_fix_nostim).states_onset)*1000);
-states_mem_left_stim = cat(1, trial(idx_mem_left_stim).states);
-states_onset_mem_left_stim = round(cat(1, trial(idx_mem_left_stim).states_onset)*1000);
-states_mem_left_nostim = cat(1, trial(idx_mem_left_nostim).states);
-states_onset_mem_left_nostim = round(cat(1, trial(idx_mem_left_nostim).states_onset)*1000);
-states_mem_right_stim = cat(1, trial(idx_mem_right_stim).states);
-states_onset_mem_right_stim = round(cat(1, trial(idx_mem_right_stim).states_onset)*1000);
-states_mem_right_nostim = cat(1, trial(idx_mem_right_nostim).states);
-states_onset_mem_right_nostim = round(cat(1, trial(idx_mem_right_nostim).states_onset)*1000);
-
-% get states and state onsets for all rewarded trials
-states_reward_fix = cat(1, trial(idx_fix_success).states);
-states_onset_reward_fix = round(cat(1, trial(idx_fix_success).states_onset)*1000);
-states_reward_mem = cat(1, trial(idx_mem_success).states);
-states_onset_reward_mem = round(cat(1, trial(idx_mem_success).states_onset)*1000);
-
-% get states and state onsets for aborted trials
-% states_abort = [];
-states_onset_abort = [];
-n_trial_abort = 0;
-for t = idx_abort % loop through all aborted trials
-    if strcmp(trial(t).abort_code, 'ABORT_EYE_FIX_ACQ_STATE') == 0
-        n_trial_abort = n_trial_abort + 1;
-        %     states_abort(n_trial_abort,1) = trial(t).states(1);
-        %     states_abort(n_trial_abort,2) = trial(t).states(end);
-        states_onset_abort(n_trial_abort,1) = round(trial(t).states_onset(1)*1000);
-        states_onset_abort(n_trial_abort,2) = round(trial(t).states_onset(end)*1000);
+for k = 1 : length(trial)
+    
+    % defining, what choice trials are 
+    if trial(k).task.correct_choice_target == [1 2]
+        trial(k).choice = {'choice'};
+    elseif trial(k).task.correct_choice_target == 1
+        trial(k).choice = {'instructed'};
     end
+ 
+
+    % target chosen
+    if ~isnan(trial(k).target_selected)
+        
+        if trial(k).effector == 3 % get only saccade trials
+            whichtarget = trial(k).target_selected(1); % here in saccade trials pic the number (can be 1 or 2) which is FIRST in target_selected (because it is for saccades)
+            
+            if       trial(k).eye.tar(whichtarget).pos(1) < 0
+                trial(k).target_chosen = {'left'};
+            elseif   trial(k).eye.tar(whichtarget).pos(1) > 0
+                trial(k).target_chosen = {'right'};
+            end
+            
+        elseif trial(k).effector == 4 % get only reach trials
+            whichtarget = trial(k).target_selected(2); % here in reach trials pic the number (its value can be 1 or 2) which is SECOND in target_selected (because it is for reaches)
+            
+            if       trial(k).hnd.tar(whichtarget).pos(1) < 0
+                 trial(k).target_chosen = {'left'};
+            elseif   trial(k).hnd.tar(whichtarget).pos(1) > 0
+                 trial(k).target_chosen = {'right'};
+            end
+        end % if for which effector
+
+    else
+         trial(k).target_chosen = {'none'};
+        
+    end  % if for chosen target or not
+ 
+    
 end
 
 
-%% add predictors to protocol
-% predictor: fixation without microstimulation
-prtpreds(1).onset = states_onset_fix_nostim(states_fix_nostim == 3) + fix_hold + cue_hold; % from time corresponding to beginning of memory period/stimulation
-prtpreds(1).offset = states_onset_fix_nostim(states_fix_nostim == 3) + fix_hold + cue_hold + mem_hold - offset_saccade; % to time corresponding to end of (relevant part) of memory period
 
-% predictor: post-fixation without microstimulation
-prtpreds(2).onset = states_onset_fix_nostim(states_fix_nostim == 3) + fix_hold + cue_hold + mem_hold - offset_saccade + 1; % from time corresponding to end of (relevant part of) memory period
-prtpreds(2).offset = states_onset_fix_nostim(states_fix_nostim == 20); % to success feedback
+%% FILL IN "PRTPREDS" with name, onset, offset
 
-% predictor: memory saccade right without microstimulation
-prtpreds(3).onset = states_onset_mem_right_nostim(states_mem_right_nostim == 7); % from beginning of memory period
-prtpreds(3).offset = states_onset_mem_right_nostim(states_mem_right_nostim == 7) + mem_hold - offset_saccade; % to end of (relevant part) of memory period
+eff = unique([trial.eff_name]); % 'eye', 'hand'
+cho = unique([trial.choice]); % 'choice', 'instructed'
+sid = {'left' 'right'}; % leaving out none
+pha = {'cue' 'mem' 'mov'};
 
-% predictor: post-memory saccade right without microstimulation
-prtpreds(4).onset = states_onset_mem_right_nostim(states_mem_right_nostim == 7) + mem_hold - offset_saccade + 1; % end of (relevant part) of memory period
-prtpreds(4).offset = states_onset_mem_right_nostim(states_mem_right_nostim == 20); % to success feedback
 
-% predictor: memory saccade left without microstimulation
-prtpreds(5).onset = states_onset_mem_left_nostim(states_mem_left_nostim == 7); % from beginning of memory period
-prtpreds(5).offset = states_onset_mem_left_nostim(states_mem_left_nostim == 7) + mem_hold - offset_saccade; % to end of (relevant part) of memory period
+% [prtpreds(1:NrofPreds).n_correct_trials] = deal([]); % empty array to be filled in cumulatively
+[prtpreds(1:NrofPreds).name] = deal([]);
+[prtpreds(1:NrofPreds).onset] = deal([]);
+[prtpreds(1:NrofPreds).offset] = deal([]);
 
-% predictor: post-memory saccade left without microstimulation
-prtpreds(6).onset = states_onset_mem_left_nostim(states_mem_left_nostim == 7) + mem_hold - offset_saccade + 1; % from end of (relevant part) of memory period
-prtpreds(6).offset = states_onset_mem_left_nostim(states_mem_left_nostim == 20); % to success feedback
+runningNR = 0;
 
-% predictor: reward
-prtpreds(7).onset = sort([states_onset_reward_fix(states_reward_fix == 20); states_onset_reward_mem(states_reward_mem == 20)] + 1); % from beginning of reward period
-prtpreds(7).offset = sort([states_onset_reward_fix(states_reward_fix == 21); states_onset_reward_mem(states_reward_mem == 21)] + reward_time); % to end of ITI
+   
+for i = 1:length(eff)
 
-% predictor: abort
-prtpreds(8).onset = states_onset_abort(:,1);
-prtpreds(8).offset = states_onset_abort(:,2) + ITI_fail;
+    for k = 1:length(cho)
 
-...
+        for l = 1:length(sid)
+            
+            for m = 1:length(pha)
 
-% predictor: mov reach choice r
-prtpreds(24).onset = [573 9400]; % -> how many trials of this condition are in the run (here 2 as an example)
-prtpreds(24).offset = [800 9800];
+                runningNR =runningNR +1;
+
+                % filter out all trials with respective combined condition
+                temp = trial(...
+                    [trial.success]       == 1      & ...
+                    strcmp(eff(i),[trial.eff_name]) & ...
+                    strcmp(cho(k),[trial.choice])   & ...
+                    strcmp(sid(l),[trial.target_chosen]) );
+                
+                % put the name of combined condition in the struct
+                prtpreds(runningNR).name = char(strcat(eff(i),'_', cho(k),'_',sid(l),'_',pha(m)));
+                prtpreds(runningNR).number = runningNR;
+
+                % get the onsets for the repsective phase 
+                onset_times = [temp.states_onset];
+                
+                if     strcmp('cue',pha(m))
+                    
+                    onset_idx = [temp.states] == 6;
+                    offset_idx = [temp.states] == 7;
+                    
+                    prtpreds(runningNR).onset  = round(onset_times(onset_idx)*1000)  + cue_onset_delay;
+                    prtpreds(runningNR).offset = round(onset_times(offset_idx)*1000) + cue_offset_delay;
+                    
+                elseif strcmp('mem',pha(m))
+                    
+                    onset_idx = [temp.states] == 7;
+                    offset_idx = [temp.states] == 9;                    
+                    
+                    prtpreds(runningNR).onset  = round(onset_times(onset_idx)*1000)  + mem_onset_delay;
+                    prtpreds(runningNR).offset = round(onset_times(offset_idx)*1000) + mem_offset_delay;
+                    
+                elseif strcmp('mov',pha(m))
+                    
+                    onset_idx = [temp.states] == 9;
+                    offset_idx = [temp.states] == 10;
+                    
+                    prtpreds(runningNR).onset  = round(onset_times(onset_idx)*1000) + mov_onset_delay;
+                    prtpreds(runningNR).offset = round(onset_times(offset_idx)*1000) + mov_offset_delay;
+
+                end %if state of which phase of the trial
+           
+            end % loop phase
+        end % loop side of target (left/right)
+    end % loop choice
+end % loop effector
+
+
+
+%% COLORS
+
+% https://docplayer.org/docs-images/54/34504262/images/3-0.png
+% https://www.rapidtables.com/web/color/RGB_Color.html
+% https://pinetools.com/lighten-color
+
+%% EYE CHOICE LEFT
+% 'eye_choice_left_cue' 
+% red
+prtpreds(1).r = 227; 
+prtpreds(1).g = 35; 
+prtpreds(1).b = 34; 
+
+% 'eye_choice_left_mem'
+% red-orange
+prtpreds(2).r = 234;
+prtpreds(2).g = 98;
+prtpreds(2).b = 31;
+
+% 'eye_choice_left_mov'
+% orange
+prtpreds(3).r = 253; 
+prtpreds(3).g = 198; 
+prtpreds(3).b = 11; 
+
+
+%%
+
+% 'eye_choice_right_cue'
+prtpreds(4).r = 241;
+prtpreds(4).g = 145;
+prtpreds(4).b = 144;
+
+% 'eye_choice_right_mem'
+prtpreds(5).r = 244; 
+prtpreds(5).g = 176; 
+prtpreds(5).b = 143; 
+
+% 'eye_choice_right_mov'
+prtpreds(6).r = 254;
+prtpreds(6).g = 226;
+prtpreds(6).b = 132;
+
+%% EYE INSTRUCTED LEFT
+
+% 'eye_instructed_left_cue'
+% green
+prtpreds(7).r = 0;
+prtpreds(7).g = 142;
+prtpreds(7).b = 91;
+
+% 'eye_instructed_left_mem'
+% blue green
+prtpreds(8).r = 6;
+prtpreds(8).g = 150;
+prtpreds(8).b = 187;
+
+% 'eye_instructed_left_mov'
+% blue
+prtpreds(9).r = 42;
+prtpreds(9).g = 113;
+prtpreds(9).b = 176;
+
+%% 
+
+% 'eye_instructed_right_cue'
+prtpreds(10).r = 70;
+prtpreds(10).g = 255;
+prtpreds(10).b = 188;
+
+% 'eye_instructed_right_mem'
+prtpreds(11).r = 101;
+prtpreds(11).g = 219;
+prtpreds(11).b = 250;
+
+% 'eye_instructed_right_mov'
+prtpreds(12).r = 137;
+prtpreds(12).g = 184;
+prtpreds(12).b = 226;
+
+%% HAND CHOICE LEFT
+
+% 'hand_choice_left_cue'
+% green-yellow
+prtpreds(13).r = 140;
+prtpreds(13).g = 187;
+prtpreds(13).b = 38;
+
+% 'hand_choice_left_mem'
+% yellow
+prtpreds(14).r = 244;
+prtpreds(14).g = 229;
+prtpreds(14).b = 0;
+
+% 'hand_choice_left_mov'
+% yellow-orange
+prtpreds(15).r = 253;
+prtpreds(15).g = 198;
+prtpreds(15).b = 11;
+
+%%
+% 'hand_choice_right_cue'
+prtpreds(16).r = 201;
+prtpreds(16).g = 230;
+prtpreds(16).b = 136;
+
+% 'hand_choice_right_mem'
+prtpreds(17).r = 255;
+prtpreds(17).g = 246;
+prtpreds(17).b = 122;
+
+% 'hand_choice_right_mov'
+prtpreds(18).r = 254;
+prtpreds(18).g = 226;
+prtpreds(18).b = 132;
+
+%% HAND INSTRUCTED LEFT
+% 'hand_instructed_left_cue'
+% red-purple
+prtpreds(19).r = 196;
+prtpreds(19).g = 3;
+prtpreds(19).b = 125;
+
+% 'hand_instructed_left_mem'
+% purple
+prtpreds(20).r = 109;
+prtpreds(20).g = 57;
+prtpreds(20).b = 139;
+
+% 'hand_instructed_left_mov'
+% blue-purple
+prtpreds(21).r = 68;
+prtpreds(21).g = 78;
+prtpreds(21).b = 153;
+
+%%
+
+% 'hand_instructed_right_cue'
+prtpreds(22).r = 252;
+prtpreds(22).g = 101;
+prtpreds(22).b = 197;
+
+% 'hand_instructed_right_mem'
+prtpreds(23).r = 185;
+prtpreds(23).g = 143;
+prtpreds(23).b = 209;
+
+% 'hand_instructed_right_mov'
+prtpreds(24).r = 154;
+prtpreds(24).g = 161;
+prtpreds(24).b = 210;
+
+%%
+for i = 1:length(prtpreds)
+    prtpreds(i).name = char(prtpreds(i).name );
+end
+
 
 %% create PRT file
+
 if isempty(run_name)
     prt_fname = [pathname filesep filename  '.prt'];
 else
@@ -210,4 +357,4 @@ fclose(fid);
 
 disp(['Protocol ' prt_fname ' saved']);
 
-end
+
