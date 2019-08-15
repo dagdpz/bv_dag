c84890daba0008097fbafcf3ec7c4277e787ab58
Igor Kagan <igor.b.kagan@gmail.com>
2019-08-15 12:11:11 +0200

    name fix

--- ne_prt2avg/ne_prt2avg_reach_decision_pilot.m
+++ ne_prt2avg/ne_prt2avg_reach_decision_pilot.m
@@ -4,8 +4,11 @@
 avg_name = mfilename; % or something else
 
 % MAKE SURE CONDITIONS FIT THOSE IN PRT!
-conditions2take = {'saccade_choice_left_cue','saccade_choice_right_cue','saccade_instructed_left_cue','saccade_instructed_right_cue',...
-                   'reach_choice_left_cue','reach_choice_right_cue','reach_instructed_left_cue','reach_instructed_right_cue'};
+% conditions2take = {'saccade_choice_left_cue','saccade_choice_right_cue','saccade_instructed_left_cue','saccade_instructed_right_cue',...
+%                    'reach_choice_left_cue','reach_choice_right_cue','reach_instructed_left_cue','reach_instructed_right_cue'};
+
+conditions2take = {'sac_choi_l_cue','sac_choi_r_cue','sac_instr_l_cue','sac_instr_r_cue',...
+                   'reach_choi_l_cue','reach_choi_r_cue','reach_instr_l_cue','reach_instr_r_cue'};
 
 
 avg.ProtocolTimeResolution = 'msec';
