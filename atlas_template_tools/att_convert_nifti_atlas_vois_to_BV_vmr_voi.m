4be59744775aadd68934a1266e87f8d9e0a9493a
Igor Kagan <igor.b.kagan@gmail.com>
2020-11-11 14:47:17 +0100

    Create att_convert_nifti_atlas_vois_to_BV_vmr_voi.m

---
+++ atlas_template_tools/att_convert_nifti_atlas_vois_to_BV_vmr_voi.m
@@ -0,0 +1,32 @@
+function att_convert_nifti_atlas_vois_to_BV_vmr_voi
+%att_convert_nifti_atlas_vois_to_BV_vmr_voi - convert NIfTI vois exported by att_nifti_extract_all_nifti_vois_from_atlas to VMR and then to BV VOI
+
+if 0 % Y:\Atlases\macaque\Calabrese2015\atlas_voi
+    load civm_rhesus_v1_ontology.mat;
+    
+    N = length(ds.Value);
+    
+    global n
+    n = neuroelf;
+    
+    for k=1:N,
+        d = dir([num2str(ds.Value(k)) '_*.nii']);
+        vmr_path = ne_convert_MRIcron_nifti_voi_to_vmr(d.name);
+        ne_convert_vmr_to_voi(vmr_path,ceil(ds.rgb(k,:)*255));
+        
+    end
+end
+
+load SARMdsshort.mat;
+
+N = length(SARMkeyallds.Index);
+
+global n
+n = neuroelf;
+
+for k=1:N,
+	d = dir([num2str(SARMkeyallds.Index(k)) '_*.nii']);
+	vmr_path = ne_convert_MRIcron_nifti_voi_to_vmr(d.name);
+	ne_convert_vmr_to_voi(vmr_path);
+	
+end
