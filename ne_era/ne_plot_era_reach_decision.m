602b0fc3f334b48de6614c969b9fbc82322224a9
peterneumann <45561899+peterneumann@users.noreply.github.com>
2020-12-06 19:01:27 +0100

    Update ne_plot_era_reach_decision.m

--- ne_era/ne_plot_era_reach_decision.m
+++ ne_era/ne_plot_era_reach_decision.m
@@ -1,24 +1,53 @@
-function dt = ne_plot_era_reach_decision(era_files)
+function dt = ne_plot_era_reach_decision(era_files,subject_name,runpath)
 
-opengl software % http://www.mathworks.com/matlabcentral/answers/101588
-ini_dir = pwd;
+% opengl software % http://www.mathworks.com/matlabcentral/answers/101588
+% ini_dir = pwd;
 
+export = 1;
 
-%run('ne_era_settings');
+% %%
+% if nargin > 1
+%     era_settings_id = 'Human_reach_decision';
+% end
+% 
+% run('ne_era_settings');
+
 
 %% data1 = era.mean; %NrOfVOIs X NrOfCurves X NrOfTimePoints
 
 %% load in data
 tc = load(era_files{1}); 
 
+%% get colors
+colors = table();
+for i = 1:length(tc.era.avg.Curve);
+    te = table();
+    
+    name = char({tc.era.avg.Curve(i).Name});
+    name_parts = strsplit(name,'_');
+    
+    te.name = {[name_parts{1} '_' name_parts{2} '_' name_parts{3}]};
+    te.color = tc.era.avg.Curve(i).TimeCourseColor1;
+    
+    colors = [colors; te];
+end
+
+colors = sortrows(colors,1);
+colors.color = colors.color/255;
+
+%% load rest and concetinate
 for e = 2:length(era_files)
    ttt = load(era_files{e}); 
    tc(e) = ttt;
 end
 
+
 %%
 dt = table();
 for v = 1:size(tc(1).era.mean,1) % loop over VIOs 
+    
+    voi_name = tc(1).era.voi(v).Name;
+
     
     for d = 1:length(tc) % loop over delay files
         
@@ -50,28 +79,52 @@ for v = 1:size(tc(1).era.mean,1) % loop over VIOs
     dt.eff  = categorical(dt.eff);
     dt.choi = categorical(dt.choi);
     dt.side = categorical(dt.side); 
-    dt.trigger_point = categorical(dt.trigger); 
+    dt.trigger = categorical(dt.trigger); 
     
-    dt.upCI = dt.mean + 1*dt.sd; 
-    dt.loCI = dt.mean - 1*dt.sd;
+    dt.upCI = dt.mean + 2*dt.sd; 
+    dt.loCI = dt.mean - 2*dt.sd;
     
     %% Plot
+    clear Gsu
+    %figure ('Position', [100 100 1600 1000]);
     figure;
-    Gsu = gramm('x',dt.time,'y',dt.mean,'ymin',dt.loCI,'ymax',dt.upCI','color',dt.name,'row',dt.delay);
+    Gsu = gramm('x',dt.time,'y',dt.mean,'ymin',dt.loCI,'ymax',dt.upCI','color',dt.name,'column',dt.eff,'row',dt.delay,'subset',dt.trigger == 'cue');
     Gsu.geom_interval('geom','area');
+    Gsu.axe_property('Xlim',[-3 18],'Ylim',[-1.5 1.5]);
+    Gsu.set_order_options('row',{'3','6','9','12','15'},'color',colors.name,'column',{'sac' 'reach'});
+    Gsu.set_color_options('map',colors.color,'n_color',8,'n_lightness',1);
+    Gsu.set_names('color','','row','','column','','x','time in seconds','y','PSC');
+    Gsu.geom_vline('xintercept',0,'style','k-');
+    Gsu.geom_hline('yintercept',0,'style','k--');
+    Gsu.set_title([subject_name '_' voi_name]);
+    
+    
     Gsu.draw;
     
+    height_hline = [-1.5 1.5];
+    line([3 3],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(1));
+    line([3 3],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(6));
     
+    line([6 6],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(2));
+    line([6 6],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(7));
+
+    line([9 9],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(3));
+    line([9 9],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(8));
     
-%     test_ges = table();
-% for i = 1:8
-%     test = table();
-%     test.data = squeeze(era.mean(1,i,:));
-%     test.ind = repmat(i,151,1);
-%     test.time = era.timeaxis';
-%     test_ges = [test_ges; test];
-%     
-% end
+    line([12 12],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(4));
+    line([12 12],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(9));
+    
+    line([15 15],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(5));
+    line([15 15],[height_hline(1) height_hline(2)] ,'Color','k','Parent',Gsu.facet_axes_handles(10));
+    
+    %% export
+    if export
+        %orient('tall');
+        saveas(gcf, [runpath filesep subject_name '_' voi_name '_per_delay.pdf'], 'pdf');
+        close(gcf);  
+    end
+    
+    dt = table();
 
 
 end
