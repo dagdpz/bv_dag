d2a9aea1bb7b886da142ca5e6e5b561b900a0fb5
Igor Kagan <igor.b.kagan@gmail.com>
2021-03-08 19:00:23 +0100

    Update ne_vmp2vmr2nifti.m

--- _4NeuroElf/ne_vmp2vmr2nifti.m
+++ _4NeuroElf/ne_vmp2vmr2nifti.m
@@ -14,26 +14,39 @@ map_index = 1;
 vmp = xff(vmp_path);
 vmr = xff(vmr_path);
 
-map_data = vmp.Map(map_index).VMPData;
 
-if resample2vmr % Resample VMP to VMR
-    
-    nx = vmp.XEnd - vmp.XStart;
-    ny = vmp.YEnd - vmp.YStart;
-    nz = vmp.ZEnd - vmp.ZStart;
-    [x, y, z]= ndgrid(linspace(1, size(map_data, 1), nx), ...
-        linspace(1, size(map_data, 2), ny), ...
-        linspace(1, size(map_data, 3), nz));
-    map_data = interp3(map_data, x, y, z, 'bicubic');
+
+if resample2vmr % Resample VMP to VMR (1x1x1)
+    hvmp = vmp.MakeHiResVMP;
 end
 
 % Use (otherwise empty) vmr to keep a vmp map
+
 vmr.VMRData = vmr.VMRData*0;
-vmr.VMRData(vmp.XStart:vmp.XEnd-1, ...
-            vmp.YStart:vmp.YEnd-1, ...
-            vmp.ZStart:vmp.ZEnd-1) = map_data;
+vmr.VMRData(vmp.XStart:vmp.XEnd, ...
+            vmp.YStart:vmp.YEnd, ...
+            vmp.ZStart:vmp.ZEnd) = hvmp.Map(map_index).VMPData; %permute(map_data,[2, 3, 1]); % Y Z X 
+        
+% map_data = vmp.Map(map_index).VMPData;
+
+% if resample2vmr % Resample VMP to VMR
+%     
+%     nx = vmp.XEnd - vmp.XStart;
+%     ny = vmp.YEnd - vmp.YStart;
+%     nz = vmp.ZEnd - vmp.ZStart;
+%     [x, y, z]= ndgrid(linspace(1, size(map_data, 1), nx), ...
+%         linspace(1, size(map_data, 2), ny), ...
+%         linspace(1, size(map_data, 3), nz));
+%     map_data = interp3(map_data, x, y, z, 'bicubic');
+% end
+
+% % Use (otherwise empty) vmr to keep a vmp map
+% vmr.VMRData = vmr.VMRData*0;
+% vmr.VMRData(vmp.XStart:vmp.XEnd-1, ...
+%             vmp.YStart:vmp.YEnd-1, ...
+%             vmp.ZStart:vmp.ZEnd-1) = map_data;
         
 % vmr.SaveAs('temp.vmr');
 
 % Save
-vmr.ExportNifti([vmp_path(1:end-4) suffix '.nii'],false);
+vmr.ExportNifti([vmp_path(1:end-4) suffix '.nii'],true);
